57a341fd9b3e04674bb8280d30b1fca93b13f717
hendrtre <trentdev2020@gmial.com>
2021-02-15 15:38:05 -0700

    Finished Challange

--- src/challenge/lock-logic.js
+++ src/challenge/lock-logic.js
@@ -8,23 +8,12 @@ const lockState = window.mobx.observable({
 })
 
 function changeDialValue (index, incrementBy) {
-  // This part is missing some code
-  // This function is automatically called when the user clicks on a chevron
-  // it will be called with a wheel index and an amount to change the value by
-  // for example, if a user clicks on the "up" arrow for wheel 0
-  // this will be called with arguments (0, 1) indicating we should raise the first dial's value by one
-  // for example, if the user clicked the "down" arrow for the last wheel
-  // this will be called with arguments (3, -1).
-
-  // to change the state of the lock, simply make a call like
-  // lockState.locked = false
-  // or lockState.wheels[1] = 2
-  // the lock will re-render itself when the value changes
-
-  // When the lock is set to match the secretCombo
-  // call the redirect() function with your name
-  // eg: redirect('larry-lobster')
-  // the redirect function will only redirect if the lockState is unlocked
+  lockState.wheels[index] = lockState.wheels[index] + incrementBy  
+  if (lockState.wheels.every((v, i) => v === SECRET_COMBO[i])) {
+    lockState.locked = false
+    console.log(lockState.wheels)
+    redirect('Trent-Hendrickson')
+  } 
 }
 
 // let our other modules find our functions
